02756f16b146eccfca409269dcf9c2aeb997c8c2
Rodrigo Augusto <rodrigoaugusto839@gmail.com>
2021-01-23 21:40:40 -0300

    Classe Vendedor

--- src/aplication/Programa.java
+++ src/aplication/Programa.java
@@ -1,12 +1,24 @@
 package aplication;
 
+
+
+
+
+
+
+import java.util.Date;
+
 import model.entidade.Departamento;
+import model.entidade.Vendedor;
 
 public class Programa {
 	
 	public static void main(String[] args) {
 		Departamento obj=new Departamento(1, "Livros");
-		System.out.println(obj);
+		
+		Vendedor vendedor=new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		System.out.println(vendedor);
+		 
 	}
 
 }
