602e73ed4004e7bbb15416012617fb800382005d
Rodrigo Augusto <rodrigoaugusto839@gmail.com>
2021-01-23 21:16:13 -0300

    Classe Departamento

---
+++ src/aplication/Programa.java
@@ -0,0 +1,12 @@
+package aplication;
+
+import model.entidade.Departamento;
+
+public class Programa {
+	
+	public static void main(String[] args) {
+		Departamento obj=new Departamento(1, "Livros");
+		System.out.println(obj);
+	}
+
+}
